7ff6d3a421c3b6510a04c691b2de14f49124b075
Maxim <www.bigstar.promax@gmail.com>
2024-01-15 13:20:45 +0700

    Практическая работа 4

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ let app = new Vue({
         product: "Socks",
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-       // inStock: true,
-        onsale: true,
-        inventory: 1
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
     }
 })
