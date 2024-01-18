e768d35fc034f90a4485da341ffe0f5494d5d9fb
Maxim <www.bigstar.promax@gmail.com>
2024-01-18 09:25:05 +0700

    v0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-let product = "socks";
+Vue.component('column', {
+    template: `
+     <div class="column" id="column1">
+            <h2>column 1</h2>
+            <div class="card">
+                <h3>card 1</h3>
+                <ul class="list">
+                    <li class="list-item">
+                        <input type="checkbox" class="checkbox">check 1</li>
+                    <li class="list-item">
+                        <input type="checkbox" class="checkbox">check 2</li>
+                    <li class="list-item">
+                        <input type="checkbox" class="checkbox">check 3</li>
+                </ul>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            name: null
+        }
+    }
+})
+
 let app = new Vue({
     el: '#app',
     data: {
